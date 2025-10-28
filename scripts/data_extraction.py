a6710dfeb0e32d6d7322f4587169a05922286bb0
musyani-io <musyani.io67@gmail.com>
2025-10-28 18:15:02 +0300

    Rectified a new iteration way

--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -10,7 +10,7 @@ def envSetup(sourcePath):
     # Returns the required worksheet to work at that time!
 
     date = datetime(2024, 8, 22)  # Dummy date for configuration, will be erased later
-    # date = datetime.today()  # This will be enable on final testing and usage
+    # date = datetime.today()  # This will be enabled on final testing and usage
 
     try:
 
@@ -30,74 +30,25 @@ def extractFromBox(cell):
 
     def noneReturn(value, exceptionValue):
         return exceptionValue if value is None else value
+    
 
-    # Extract communication type, name and telephone
-    commCell = jumpTo(0, 3)
-    nameCell = jumpTo(1, 1)
-    numCell = jumpTo(1, 3)
-    numCell.value = localToInt(numCell.value)
+def iterateOnBoxes(cell):
 
-    # Extract reading date
-    readingDate = jumpTo(2, 4)
-    readingDate.value = noneReturn(readingDate.value, datetime(2000, 1, 1))
-    readingDate.value = datetime.strftime(readingDate.value, "%d-%b-%Y")
-
-    # Extract amount in liters used, net charge and adjustments
-    literUsage = jumpTo(5, 4)
-    netCharge = jumpTo(6, 4)
-    adjustments = jumpTo(7, 4)
-
-    # Extract final bill
-    bill = jumpTo(11, 1)
-
-    # Extract customer's color (location varied)
-    topColor = cell.border.top.color
-    color = topColor.index if topColor else None
-    if color is None:
-        location = "Lumo"
-    else:
-        location = "Chanika"
-
-    return (
-        readingDate.value,
-        nameCell.value,
-        numCell.value,
-        commCell.value,
-        location,
-        round(noneReturn(literUsage.value, 0), 1),
-        int(noneReturn(netCharge.value, 0)),
-        0 if adjustments.value is None else adjustments.value,
-        noneReturn(bill.value, 0),
-    )
-
-
-def iterateOverBoxes(startCell):
-    # As the name, goes over a fixed increment to collect data to other boxes
-
-    usedCell = startCell
-    vertCustomers = 8
+    rows = 50
     customerInfo = []
-    rowIncr = 14
-    colIncr = 6
-    count = 0
+    try:
 
-    while vertCustomers > 0:  # Iterate going downwards
+        while rows > 0:
+            
+            customerInfo.append(cell)
+            cell = cell.offset(row=1, column=0)
+            rows -= 1
 
-        horzCustomers = 3
-        while horzCustomers > 0:  # Iterate sideways
+        return customerInfo
 
-            customerInfo.append(extractFromBox(usedCell))
-
-            usedCell = usedCell.offset(row=0, column=colIncr)
-            horzCustomers -= 1
-
-        count += 1
-        usedCell = startCell
-        usedCell = usedCell.offset(row=(count * rowIncr), column=0)
-        vertCustomers -= 1
-
-    return customerInfo
-
+    except Exception as Error:
+        print(f"Error: {type(Error).__name__} - {Error}")
+    
 
 if __name__ == "__main__":
 
@@ -108,10 +59,10 @@ if __name__ == "__main__":
         workSheet, fileName = envSetup(sourceFilePath)  # Require sheet given
 
         startCell = workSheet["A1"]
-        customerInfo = iterateOverBoxes(startCell)  # Iterated over box to collect data
-
-        fileCreation(fileName)
-        addCsvData(fileName, customerInfo)
+        
+        print(iterateOnBoxes(startCell))
+        # fileCreation(fileName)
+        # addCsvData(fileName, customerInfo)
 
     else:
         print("Invalid path provided!")
